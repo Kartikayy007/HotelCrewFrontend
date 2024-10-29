348ebb003e1a3cf0da840ee2e610af90c9efcb45
Navya Gupta <navyagupta171@gmail.com>
2024-10-30 00:50:44 +0530

    labels removed and error detection  added

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,40 +1,88 @@
-import { useRef, useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import arrow from '../assets/arrow.svg';
 import { useDispatch, useSelector } from "react-redux";
 import { registerUser } from "../redux/slices/UserSlice"
 import Frame from '../assets/Frame.svg'
 import eye from '../assets/eye.svg';
+import eyeClosed from '../assets/eyeClosed.svg';
 
-
+// const pwdRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,12}$/;
+// const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 const SignUp = () => {
-    const userRef = useRef();
+
     const dispatch = useDispatch();
     const { loading, error } = useSelector((state) => state.user);
+
     const [user, setUser] = useState('');
-   
-    const [userFocus, setUserFocus] = useState(false);
 
     const [email, setEmail] = useState('');
-    const [emailFocus, setEmailFocus] = useState(false);
+    // const [validEmail, setValidEmail] = useState(false);
 
     const [pwd, setPwd] = useState('');
-    const [showPassword, setShowPassword] = useState(false)
-    const [pwdFocus, setPwdFocus] = useState(false);
+    const [showPwd, setShowPwd] = useState(false)
+    // const [validPwd, setValidPwd] = useState(false);
 
     const [matchPwd, setMatchPwd] = useState('');
-    const [showMatchPassword, setShowMatchPassword] = useState(false)
-    const [matchFocus, setMatchFocus] = useState(false);
+    const [showMatchPwd, setShowMatchPwd] = useState(false)
+    // const [validMatch, setValidMatch] = useState(false);
 
-    const [showError, setShowError] = useState(false);
+    const [errorMsg, setErrorMsg] = useState('');
 
-    useEffect(() => {
-        userRef.current.focus();
-    }, [])
+    const handleInputChange = (set) => (e) => {
+        set(e.target.value);
+        setErrorMsg(""); // Clear the error message on input change
+    };
 
-    
+    // useEffect(() => {
+    //     setValidEmail(emailRegex.test(email));
+    // }, [email])
+
+    // useEffect(() => {
+    //     setValidPwd(pwdRegex.test(pwd));
+    //     setValidMatch(pwd === matchPwd);
+    // }, [pwd, matchPwd])
+
+
     const handleSubmit = async (e) => {
         e.preventDefault();
+        // if (!validEmail) {
+        //     setErrorMsg("Invalid Email");
+        //     return;
+        // }
+        // if (!validPwd) {
+        //     setErrorMsg("Password must be 8 character long");
+        //     return;
+        // }
+        // if (!validMatch) {
+        //     setErrorMsg("Passwords do not match");
+        //     return;
+        // }
+        if (!user || !email || !pwd || !matchPwd) {
+            setErrorMsg("Enter all fields");
+            return;
+        }
+
+        // Email format validation
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            setErrorMsg("Invalid email");
+            return;
+        }
+
+        // Password length validation
+        // if (pwd.length < 6) {
+        //     setErrorMsg("Password must be 6 characters long");
+        //     return;
+        // }
+
+        // Password match validation
+        if (!(pwd === matchPwd)) {
+            setErrorMsg("Passwords do not match");
+            return;
+        }
+
+        setErrorMsg("");
         const userCredentials = {
             email: email,
             password: pwd,
@@ -43,10 +91,11 @@ const SignUp = () => {
         dispatch(registerUser(userCredentials)).then((result) => {
             if (registerUser.fulfilled.match(result)) {
                 setEmail("");
+                setUser("");
                 setPwd("");
-
+                setMatchPwd("");
                 console.log('registered');
-               
+
             }
         });
     }
@@ -59,117 +108,86 @@ const SignUp = () => {
               ">
                     Register
                 </h1>
-                <form onSubmit={handleSubmit} className=" w-[303px] h-[220px] absolute top-[201px]  gap-2 flex flex-col p-1 ">
-                    <div className={`relative w-full ${userFocus ? 'shift-up' : ''}`}>
-                        <label htmlFor="username" className={`transform -translate-y-1/2 transition-opacity duration-200 ${userFocus ? 'opacity-100' : 'opacity-0'
-                            } text-[24px] font-medium text-left`}
-                        >
-                            Name
-                        </label>
+                <form onSubmit={handleSubmit} className=" w-[303px] h-[220px] absolute top-[201px]  gap-9 flex flex-col p-1 mb-0">
+                    <div className={`relative w-full`}>
+
                         <input
                             type="text"
                             id="username"
-                            placeholder={userFocus ? "" : "Name"}
-                            ref={userRef}
+                            placeholder={(errorMsg === "Enter all fields" && !user) ? "Enter Name" : "Name"}
                             autoComplete="off"
-                            onChange={(e) => setUser(e.target.value)}
+                            // onChange={(e) => setUser(e.target.value)}
+                            onChange={handleInputChange(setUser)}
                             value={user}
-                            required
-                            onFocus={() => setUserFocus(true)}
-                            onBlur={() => setUserFocus(false)}
-                            className={`w-full border-b-2 transition duration-200 
-                         focus:outline-none focus:ring-0 
-                        ${userFocus ? 'pb-2' : 'pb-0'}  
-                    `}
+                            className={`w-full border-b transition duration-200 
+                         focus:outline-none focus:ring-0 text-xs pl-4 pr-4 p-2 ${errorMsg === "Enter all fields" && !user ? 'border-red-500 placeholder-red-500' : 'border-gray-500  placeholder-gray-500'}`}
                         />
                     </div>
-                    <div className={`relative w-full ${emailFocus ? 'shift-up' : ''}`}>
-                        <label htmlFor="email" className={`transform -translate-y-1/2 transition-opacity duration-200 ${emailFocus ? 'opacity-100' : 'opacity-0'
-                            } text-[24px] font-medium text-left`}
-                     
-                        >
-                            Email
-                        </label>
+                    <div className={`relative w-full`}>
+
                         <input
                             type="email"
                             id="email"
                             autoComplete="off"
-                            placeholder={emailFocus ? "" : "Email"}
-                            onChange={(e) => setEmail(e.target.value)}
+                            placeholder={(errorMsg === "Enter all fields" && !email) ? "Enter E-mail" : "E-mail"}
+                            // onChange={(e) => setEmail(e.target.value)}
+                            onChange={handleInputChange(setEmail)}
                             value={email}
-                            required
-                            onFocus={() => setEmailFocus(true)}
-                            onBlur={() => setEmailFocus(false)}
-                            className={`w-full border-b-2 transition duration-200 
-                         focus:outline-none focus:ring-0 
-                        ${emailFocus ? 'pb-2 mt-4' : 'pb-0 mt-0'}   
-                    `}
+                            className={`w-full border-b transition duration-200 
+                         focus:outline-none focus:ring-0 text-xs pl-4 pr-4 p-2  ${(errorMsg === "Invalid email" && !email) || (errorMsg === "Enter all fields" && !email) ? 'border-red-500  placeholder-red-500' : 'border-gray-500  placeholder-gray-500'}`}
                         />
                     </div>
-                    <div className={`relative w-full ${pwdFocus ? 'shift-up' : ''}`}>
-                        <label htmlFor="pwd" className={`transform -translate-y-1/2 transition-opacity duration-200 ${pwdFocus ? 'opacity-100' : 'opacity-0'
-                            } text-[24px] font-medium text-left`}
 
-                        >
-                            Password
-                        </label>
-                        <div className="relative w-full">
-                            <input
-                                type={showPassword ? "text" : "password"}
-                                id="pwd"
-                                placeholder={pwdFocus ? "" : "Password"}
-                                onChange={(e) => setPwd(e.target.value)}
-                                value={pwd}
-                                required
-                                onFocus={() => setPwdFocus(true)}
-                                onBlur={() => setPwdFocus(false)}
-                                className={`w-full border-b-2 transition duration-200 
-                        focus:outline-none focus:ring-0 
-                        ${pwdFocus ? 'pb-2' : 'pb-0'}  
-                    `}
-                            />
-                            <span
-                                onClick={() => setShowPassword(!showPassword)}
-                                className="absolute right-0 top-1/3 transform -translate-y-1/2 cursor-pointer pr-2">
-                                <img src={eye} alt="Toggle Password Visibility" />
-                            </span>
-                        </div>
+                    <div className="relative w-full">
+                        <input
+                            type={showPwd ? "text" : "password"}
+                            id="pwd"
+                            placeholder={(errorMsg === "Enter all fields" && !pwd) ? "Enter Password" : "Password"}
+                            // onChange={(e) => setPwd(e.target.value)}
+                            onChange={handleInputChange(setPwd)}
+                            value={pwd}
+                            maxLength={12}
+                            className={`w-full border-b transition duration-200 
+                        focus:outline-none focus:ring-0 text-xs pl-4 pr-4 p-2 ${(errorMsg === "Enter all fields" && !pwd) ? 'border-red-500  placeholder-red-500' : 'border-gray-500 placeholder-gray-500'}`}
+                        />
+                        <span
+                            onClick={() => setShowPwd(!showPwd)}
+                            className="absolute right-0 top-1/3 transform -translate-y-1/2 cursor-pointer pr-2">
+                            <img src={showPwd ? eye : eyeClosed} alt="Toggle Password Visibility" />
+                        </span>
                     </div>
-                    <div className={`relative w-full ${matchFocus ? 'shift-up' : ''}`}>
-                        <label htmlFor="confirm_pwd" className={`transform -translate-y-1/2 transition-opacity duration-200 ${matchFocus ? 'opacity-100' : 'opacity-0'
-                            } text-[24px] text-left font-medium  `}
-                            >
-                            Confirm Password
-                        </label>
-                        <div className="relative w-full">
-                            <input
-                                type={showMatchPassword ? "text" : "password"}
-                                id="confirm_pwd"
-                                placeholder={matchFocus ? "" : "Confirm Password"}
-                                onChange={(e) => setMatchPwd(e.target.value)}
-                                value={matchPwd}
-                                required
-                                onFocus={() => setMatchFocus(true)}
-                                onBlur={() => setMatchFocus(false)}
-                                className={`w-full border-b-2 transition duration-200 
-                       focus:outline-none focus:ring-0 
-                        ${matchFocus ? 'pb-2' : 'pb-0'}  
-                    `}
-                            />
-                            <span
-                                onClick={() => setShowMatchPassword(!showMatchPassword)}
-                                className="absolute right-0 top-1/3 transform -translate-y-1/2 cursor-pointer pr-2">
-                                <img src={eye} alt="Toggle Password Visibility" />
-                            </span>
-                        </div>
+
+                    <div className={`relative w-full `}>
+
+
+                        <input
+                            type={showMatchPwd ? "text" : "password"}
+                            id="confirm_pwd"
+                            placeholder="Confirm Password"
+                            // onChange={(e) => setMatchPwd(e.target.value)}
+                            onChange={handleInputChange(setMatchPwd)}
+                            value={matchPwd}
+                            maxLength={12}
+                            className={`w-full border-b transition duration-200 
+                       focus:outline-none focus:ring-0 text-xs pl-4 pr-4 p-2 ${(errorMsg === "Passwords do not match" && pwd !== matchPwd) | (errorMsg === "Enter all fields" && !matchPwd) ? 'border-red-500 placeholder-red-500' : 'border-gray-500 placeholder-gray-500'}`}
+                        />
+                        <span
+                            onClick={() => setShowMatchPwd(!showMatchPwd)}
+                            className="absolute right-0 top-1/3 transform -translate-y-1/2 cursor-pointer pr-2 ">
+                            <img src={showMatchPwd ? eye : eyeClosed} alt="Toggle Password Visibility" />
+                        </span>
+
+                    </div>
+                    <div className={`h-[1px] `}>
+                        {errorMsg && <div className="text-red-500 text-sm">{errorMsg}</div>}
                     </div>
                     <button
                         type="submit"
                         disabled={loading}
-                        className="w-full lg:w-[88px] h-[88px] bg-[#5663AC] text-white rounded-lg flex items-center justify-center hover:bg-[#6773AC] transition-colors ml-[215px] top-10 relative"
+                        className="w-[88px] h-[88px] bg-[#5663AC] text-white rounded-lg flex items-center justify-center hover:bg-[#6773AC] transition-colors ml-[215px] top-0.2 relative"
                     >
                         {loading ? (
-                            <div className="animate-spin rounded-full h-6 w-6 border-b-2 border-white" />
+                            <div className="animate-spin rounded-full h-20 w-20 border-b-2 border-white" />
                         ) : (
                             <img src={arrow} alt="Submit" />
                         )}
