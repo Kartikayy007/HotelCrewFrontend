27dc4af910af28a5754012f3257a6a0b6a38099e
Navya Gupta <navyagupta171@gmail.com>
2024-11-23 00:22:37 +0530

    Staff Layout created

--- src/components/Staff/SLayout.jsx
+++ src/components/Staff/SLayout.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import { useSelector, useDispatch } from 'react-redux';
+import React from 'react';
+import { setCurrentComponent } from "../../redux/slices/ManagerSlice";
+import SSidebar from './StaffComponents/SSideBar';
+import SDashboard from './StaffComponents/SDashboard'
+import STask from './StaffComponents/STask';
+import SSchedule from './StaffComponents/SSchedule';
+import SProfile from './StaffComponents/SProfile';
+
 
 const SLayout = () => {
-  return (
-    <div>SLayout</div>
-  )
-}
+    const dispatch = useDispatch();
+    const componentMap = {
+        SDashboard: <SDashboard />,
+        SSchedule: <SSchedule />,
+        SProfile: <SProfile />,
+        STask: <STask />,
+        
+    };
+    const currentComponent = useSelector((state) => state.manager.currentComponent);
 
-export default SLayout
+    const handleMenuItemClick = (component) => {
+        dispatch(setCurrentComponent(component)); // Update the Redux state
+    };
+
+    return (
+        <div className="flex h-screen">
+            <SSidebar onMenuItemClick={handleMenuItemClick} />
+            <div className="flex-1 bg-[#e6eef9] overflow-x-hidden scrollbar-thin scrollbar-thumb-gray-500 scrollbar-thumb-rounded-full scrollbar-track-gray-100">
+                {/* {currentComponent
+                    ? React.createElement(currentComponent)
+                    : <div>Component not found</div>} */}
+                    {componentMap[currentComponent] || <div>Component not found</div>}
+            </div>
+        </div>
+    );
+};
+
+export default SLayout;
