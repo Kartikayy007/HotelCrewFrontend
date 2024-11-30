4ed469bb21c3f7d3f0dea14acf50fe2b4b4e38bb
Kartikay Singh pundir <130781246+kartikay6969@users.noreply.github.com>
2024-11-30 08:20:02 +0530

    refactor: remove unused component imports from main entry file

--- src/main.jsx
+++ src/main.jsx
@@ -4,22 +4,11 @@ import { Provider } from 'react-redux'
 import store from './redux/Store.js'
 import './index.css'
 import App from './App.jsx'
-import Admin from './components/admin/Admin.jsx'
-import MainLayout from './components/Manager/MainLayout.jsx'
-import Reception from './components/receptionist/Reception.tsx'
-import SLayout from './components/Staff/SLayout.jsx'
-// import IncompleteRegisteration from './components/common/IncompleteRegisteration.jsx'
-// import { RevealBento } from './components/common/IncompleteRegisteration.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
       <App />
-      {/* <RevealBento /> */}
-      {/* <MainLayout /> */}
-      {/* <Admin />    */}
-      {/* <Reception /> */}
-      {/* <IncompleteRegisteration /> */}
     </Provider>
   </StrictMode>,
 )
--- src/redux/slices/leaveSlice.jsx
+++ src/redux/slices/leaveSlice.jsx
@@ -68,6 +68,7 @@ export const updateLeaveStatus = createAsyncThunk(
     }
   }
 );
+ 
 
 // **Slice**
 const leaveSlice = createSlice({
