e64853df43375ea8b859758661b908ae057c5dc9
Kartikay Singh pundir <130781246+kartikay6969@users.noreply.github.com>
2024-11-30 09:57:00 +0530

    feat: retrieve authentication token from local storage instead of hardcoded value

--- src/redux/slices/StaffAttendanceSlice.jsx
+++ src/redux/slices/StaffAttendanceSlice.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Function to retrieve the authentication token
 const getAuthToken = () => {
-  const token  = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzM1MzI2OTkxLCJpYXQiOjE3MzI3MzQ5OTEsImp0aSI6IjNmMTM5OTIyZmYzZjQ5ZDhiOWYyOGM5ZWM1NTAzODBkIiwidXNlcl9pZCI6MTc1fQ.xn3Xra_d-fyzTxUhJFG8GpruS2scKPP2V0XmtaNT8kA';
+  const token  = localStorage.getItem('token');
   if (!token) {
     throw new Error('Authentication token not found');
   }
